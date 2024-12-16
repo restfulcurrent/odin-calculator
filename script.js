5cbacb60953edae1aca706c9e347a0f8a5f20649
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-16 15:38:48 -0500

    Implement functions for basci math operators + - * /

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1. Functions for basic math operators + - * /
+
+function add(augend, addend) {
+    return augend + addend;
+}
+
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend
+}
+
+function multiply(multiplicand, multiplier) {
+    return multiplicand * multiplier;
+}
+
+// Returns "Undefined" if we attempt to divide by zero
+function divide(dividend, divisor) {
+    return (divisor === 0) ? "Undefined" : dividend / divisor;
+}
+
+// Cutoff large numbers so that they don't overflow the display
+// That depends on the size of the display. Implement later
